b651f4482535495cfb11f14174bc707ca5ad7e23
Dohun Lee <vbn9302@gmail.com>
2025-09-05 10:31:00 -0700

    Thread sync

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,45 +6,56 @@
 
 #include <thread>
 #include <atomic>
+#include <mutex>
+#include <Windows.h>
 
-atomic<int32> sum = 0;
+mutex m;
+queue<int32> q;
+HANDLE handle;
 
-void Add() {
-	for (int32 i = 0; i < 100000000; i++) {
-		sum += 1;
+void Producer() {
+	while (true) {
+		{
+			unique_lock<mutex> lock(m);
+			q.push(100);
+		}
+
+		::SetEvent(handle); // 이벤트 신호 상태로 변경
+		this_thread::sleep_for(chrono::milliseconds(10000));
 	}
 }
 
-void Sub() {
-	for (int32 i = 0; i < 100000000; i++) {
-		sum -= 1;
+void Consumer() {
+	while (true) {
+		{
+			// 이벤트가 발생 하는 경우에만 해당 스레드를 깨워서 행동을 실행
+			::WaitForSingleObject(handle, INFINITE); // 이벤트가 신호 상태가 될 때까지 대기
+			// 이벤트가 신호 상태가 되면 자동으로 리셋됨
+			// Manual-Reset 이벤트는 수동으로 리셋해야 함
+			// ::ResetEvent(handle); // 수동 리셋
+			unique_lock<mutex> lock(m);
+			if (!q.empty()) {
+				int32 v = q.front();
+				q.pop();
+				cout << "Consumed: " << v << endl;
+			}
+		}
 	}
 }
+
 
 int main()
 {
-	Add();
-	Sub();
-
-	std::cout << sum << std::endl;
-
-	std::thread t1(Add);
-	std::thread t2(Sub);
+	// 커널 오브젝트 -> 커널에서 관리하는 객체
+	// Usage count
+	// Singnal, None-Singnal
+	// Auto -Reset, Manual-Reset
+	handle = ::CreateEvent(NULL /*보안 속성*/, FALSE /*리셋 방법*/, FALSE /*초기 시그널 상태*/, NULL);
+	thread t1(Producer);	
+	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
 
-	std::cout << sum << std::endl;
-
+	::CloseHandle(handle);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
