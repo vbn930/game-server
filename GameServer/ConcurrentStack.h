dc38d2377ae024468e715b001304492e30520a52
Dohun Lee <vbn9302@gmail.com>
2025-11-10 22:44:14 -0800

    Lock free stack
    Add LockFreeStack class for thread-safe stack operations
    
    This commit introduces a new `LockFreeStack` class that implements a lock-free stack data structure using atomic operations. It features a nested `Node` structure for data storage and a `CountedNodePtr` for reference counting. The `Push` method allows for adding elements, while `TryPop` retrieves them. A private method, `IncreaseHeadCount`, manages the reference count of the head node, ensuring thread safety without traditional locks.

--- GameServer/ConcurrentStack.h
+++ GameServer/ConcurrentStack.h
@@ -41,3 +41,50 @@ private:
 	condition_variable _conVar;
 };
 
+template<typename T>
+class LockFreeStack {
+	struct Node;
+
+	struct CountedNodePtr {
+		int32 externalCount = 0;
+		Node* ptr = nullptr;
+	};
+
+	struct Node {
+		Node(const T& value) : data(make_shared<T>(value))
+		{}
+
+		shared_ptr<T> data;
+		atomic<int32> internalCount = 0;
+		CountedNodePtr next;
+	};
+
+public:
+
+	void Push(const T& value) {
+		CountedNodePtr node;
+		node.ptr = new Node(value);
+		node.externalCount = 1;
+
+		node.ptr->next = _head;
+		while (_head.compare_exchange_weak(node.ptr->next, node) == false);
+	}
+
+	shared_ptr<T> TryPop() {
+		CountedNodePtr oldHead = _head;
+	}
+
+private:
+
+	void IncreaseHeadCount(CountedNodePtr& oldCounter) {
+		while (true) {
+			CountedNodePtr newCounter = oldCounter;
+			++newCounter.externalCount;
+
+			if (_head.compare_exchange_strong(oldCounter, newCounter)) {
+				break;
+			}
+		}
+	}
+	atomic<CountedNodePtr> _head;
+};
